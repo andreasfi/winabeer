145ec446ec1e67a1208fe1ac0c4ff365b1f8cda3
anfi <andreasfi@me.com>
2015-09-22 16:06:35 +0000

    test3

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+function sessionCheck(){
+	$usedip = getUsedIP();
+	
+}
+
 function get_client_ip() {
 	$ipaddress = '';
 	if ($_SERVER['HTTP_CLIENT_IP'])
@@ -18,11 +23,21 @@ function get_client_ip() {
 	return $ipaddress;
 }
 
+function sec_session_start() {
+    
+	//setcookie("checkin",$session_name, $date_of_expiry ); 
+	//$cookieParams = session_get_cookie_params();
+    //session_set_cookie_params("checkin", $session_name, $cookieParams["lifetime"], $secure, $httponly);
+               // Start the PHP session 
+    //session_regenerate_id(true);    // regenerated the session, delete the old one. 
+}
+
 /*
  * 
  * Database connections
  * 
  */
+ 
 function getUsedIP(){
 	try {
 		include './database-config.php';
@@ -36,23 +51,45 @@ function getUsedIP(){
 		exit();
 	}
 }
-function addUsedIp($clientIp){
-	$count = 0;
+
+function addUsedIp($clientIp, $session_name){
+	$count = 1;
+	
 	try {
 		include './database-config.php';
 		$stmt = $db->prepare("INSERT INTO usedip (
+								sessionid,
 								usedip,
 								counter)
 								VALUES (
+								:sessionid,
 								:usedip,
 								:counter)");
 	
+		$stmt->bindParam(':sessionid', $session_name);
 		$stmt->bindParam(':usedip', $clientIp);
 		$stmt->bindParam(':counter', $count);
-		//	$stmt->execute();
+		$stmt->execute();
 	} catch( PDOException $Exception ) { // Gestion des erreurs
 		// Affichage de l'erreur PDO
 		echo $Exception->getMessage();
 		exit();
 	}
+}
+
+function itterTry($client){
+	$client['counter']++;
+	try {	
+			include './database-config.php';
+			$stmt = $db->prepare("UPDATE usedip SET
+										counter = :count
+										WHERE id = :id");		
+			$stmt->bindParam(':count', $client['counter']);
+			$stmt->bindParam(':id', $client['id']);
+			$stmt->execute();	
+    		//header("Location: ./index.php");
+	} catch( PDOException $Exception ) {
+	    echo $Exception->getMessage();
+		exit();
+	}		
 }
--- index.php
+++ index.php
@@ -1,54 +1,63 @@
 <?php
 include 'functions.php';
+$alreadyused = false;
 
-session_start();
-$session_name = 'sec_session_'.$_SERVER['REMOTE_ADDR'];   // Set a custom session name
+
+	$session_name = 'sec_session_'.$_SERVER['REMOTE_ADDR'];   // Set a custom session naem
+    $secure = SECURE;
+    // This stops JavaScript being able to access the session id.
+    $httponly = true;
+    $cookie_name = 'winabeer';
+    
+    if(isset($_COOKIE[$cookie_name])){
+    	echo 'used';
+    	$alreadyused = true;
+    } else {
+    	$cookie_value = 'win_a_beer';
+		$date_of_expiry = time() + (86400 * 1); // 86400 = 1 day
+    	setcookie($cookie_name, $cookie_value, time() + (86400 * 1), $secure, $httponl,"/");
+    }
+    session_name($session_name);
+    session_start(); 
+
+
 $secure = 'SECURE'; // This stops JavaScript being able to access the session id.
 $httponly = true; // Forces sessions to only use cookies.
 $clientIp = get_client_ip()	;
-$alreadyused = false;
+
 $count = 0;
-// Function to get the client IP address
-echo $_SERVER['REMOTE_ADDR'];
-echo "<br/>Your IP:".$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+
+echo "<br/>Your IP:".get_client_ip();
 
 // @todo Is user logged in by session
 // @todo Is user logged in by IP
-// @todo Is user logged in by Cookie 
-
-
-// Get DB for...
-
 $usedip = getUsedIP();
-// Is IP already used?
+$client = null;
 foreach ($usedip as $temp){
-	if($clientIp == $temp['ip']){
+	if($clientIp == $temp['usedip']){
 		// error already played
 		$alreadyused = true;
+		$client = $temp;
 		break;
 	}
 }
 
+// @todo Is user logged in by Cookie 
+
+
+
+// Is IP already used?
+
 if($alreadyused == false) {
 	// gamble and show win or loss
-	echo 'somethin';
+	echo ' maybe you won';
 	// Update DB with Client_IP
-	addUsedIp();
+	addUsedIp($clientIp, $session_name);
 } else {
 	// error, already played
+	echo 'already played';
 	// put counter up
+	itterTry($client);
 }
 
-if (isset($_GET["logout"])) {
-	// Unset all session values
-	$_SESSION = array();// get session parameters
-	$params = session_get_cookie_params();// Delete the actual cookie.
-	setcookie(session_name(),'', time() - 42000,$params["path"],$params["domain"],$params["secure"],$params["httponly"]);
-	session_destroy();// Destroy session
-	header('Location: ./');
-} elseif (!empty($_SESSION['username']) && ($_SESSION['logged_in'] == 1)) {
-	//echo 'yes';
-	$login = true;
-}
-
-
+echo " ------	".$_COOKIE['winabeer'];
